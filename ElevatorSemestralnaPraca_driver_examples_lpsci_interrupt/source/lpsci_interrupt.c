dea45080086f81bc57e0b6d3a64b84f051dee337
Jojo <Jojo@192.168.56.1>
2021-04-19 10:44:03 +0200

    Doplnenie funkcii Pre LED a ich testovanie.

--- ElevatorSemestralnaPraca_driver_examples_lpsci_interrupt/source/lpsci_interrupt.c
+++ ElevatorSemestralnaPraca_driver_examples_lpsci_interrupt/source/lpsci_interrupt.c
@@ -285,7 +285,312 @@ void led0IN() {
 				LPSCI_WriteBlocking(DEMO_LPSCI, LED0I, sizeof(LED0I));
 }
 
+void led1OUT() {
+	uint8_t dataPreCRC[] = {
+			dataPreCRC[0] = 0x11,
+			dataPreCRC[1] = 0x00,
+			dataPreCRC[2] = 0x01
+	};
+	uint8_t LED0O[] = {
+			LED0O[0] = dataMSG,
+			LED0O[1] = 0x11,
+			LED0O[2] = doskaADDR,
+			LED0O[3] = 0x01,
+			LED0O[4] = 0x01,
+			LED0O[5] = crc8_calc(dataPreCRC, sizeof(dataPreCRC))
+	};
+			LPSCI_WriteBlocking(DEMO_LPSCI, LED0O, sizeof(LED0O));
+}
 
+void led1IN() {
+	uint8_t dataPreCRC[] = {
+			dataPreCRC[0] = 0x21,
+			dataPreCRC[1] = 0x00,
+			dataPreCRC[2] = 0x01
+	};
+	uint8_t LED0I[] = {
+			LED0I[0] = dataMSG,
+			LED0I[1] = 0x21,
+			LED0I[2] = doskaADDR,
+			LED0I[3] = 0x01,
+			LED0I[4] = 0x01,
+			LED0I[5] = crc8_calc(dataPreCRC, sizeof(dataPreCRC))
+		};
+				LPSCI_WriteBlocking(DEMO_LPSCI, LED0I, sizeof(LED0I));
+}
+
+void led2OUT() {
+	uint8_t dataPreCRC[] = {
+			dataPreCRC[0] = 0x12,
+			dataPreCRC[1] = 0x00,
+			dataPreCRC[2] = 0x01
+	};
+	uint8_t LED0O[] = {
+			LED0O[0] = dataMSG,
+			LED0O[1] = 0x12,
+			LED0O[2] = doskaADDR,
+			LED0O[3] = 0x01,
+			LED0O[4] = 0x01,
+			LED0O[5] = crc8_calc(dataPreCRC, sizeof(dataPreCRC))
+	};
+			LPSCI_WriteBlocking(DEMO_LPSCI, LED0O, sizeof(LED0O));
+}
+
+void led2IN() {
+	uint8_t dataPreCRC[] = {
+			dataPreCRC[0] = 0x22,
+			dataPreCRC[1] = 0x00,
+			dataPreCRC[2] = 0x01
+	};
+	uint8_t LED0I[] = {
+			LED0I[0] = dataMSG,
+			LED0I[1] = 0x22,
+			LED0I[2] = doskaADDR,
+			LED0I[3] = 0x01,
+			LED0I[4] = 0x01,
+			LED0I[5] = crc8_calc(dataPreCRC, sizeof(dataPreCRC))
+		};
+				LPSCI_WriteBlocking(DEMO_LPSCI, LED0I, sizeof(LED0I));
+}
+
+void led3OUT() {
+	uint8_t dataPreCRC[] = {
+			dataPreCRC[0] = 0x13,
+			dataPreCRC[1] = 0x00,
+			dataPreCRC[2] = 0x01
+	};
+	uint8_t LED0O[] = {
+			LED0O[0] = dataMSG,
+			LED0O[1] = 0x13,
+			LED0O[2] = doskaADDR,
+			LED0O[3] = 0x01,
+			LED0O[4] = 0x01,
+			LED0O[5] = crc8_calc(dataPreCRC, sizeof(dataPreCRC))
+	};
+			LPSCI_WriteBlocking(DEMO_LPSCI, LED0O, sizeof(LED0O));
+}
+
+void led3IN() {
+	uint8_t dataPreCRC[] = {
+			dataPreCRC[0] = 0x23,
+			dataPreCRC[1] = 0x00,
+			dataPreCRC[2] = 0x01
+	};
+	uint8_t LED0I[] = {
+			LED0I[0] = dataMSG,
+			LED0I[1] = 0x23,
+			LED0I[2] = doskaADDR,
+			LED0I[3] = 0x01,
+			LED0I[4] = 0x01,
+			LED0I[5] = crc8_calc(dataPreCRC, sizeof(dataPreCRC))
+		};
+				LPSCI_WriteBlocking(DEMO_LPSCI, LED0I, sizeof(LED0I));
+}
+
+void led4OUT() {
+	uint8_t dataPreCRC[] = {
+			dataPreCRC[0] = 0x14,
+			dataPreCRC[1] = 0x00,
+			dataPreCRC[2] = 0x01
+	};
+	uint8_t LED0O[] = {
+			LED0O[0] = dataMSG,
+			LED0O[1] = 0x14,
+			LED0O[2] = doskaADDR,
+			LED0O[3] = 0x01,
+			LED0O[4] = 0x01,
+			LED0O[5] = crc8_calc(dataPreCRC, sizeof(dataPreCRC))
+	};
+			LPSCI_WriteBlocking(DEMO_LPSCI, LED0O, sizeof(LED0O));
+}
+
+void led4IN() {
+	uint8_t dataPreCRC[] = {
+			dataPreCRC[0] = 0x24,
+			dataPreCRC[1] = 0x00,
+			dataPreCRC[2] = 0x01
+	};
+	uint8_t LED0I[] = {
+			LED0I[0] = dataMSG,
+			LED0I[1] = 0x24,
+			LED0I[2] = doskaADDR,
+			LED0I[3] = 0x01,
+			LED0I[4] = 0x01,
+			LED0I[5] = crc8_calc(dataPreCRC, sizeof(dataPreCRC))
+		};
+				LPSCI_WriteBlocking(DEMO_LPSCI, LED0I, sizeof(LED0I));
+}
+
+
+void led0OUTZhasni() {
+	uint8_t dataPreCRC[] = {
+			dataPreCRC[0] = 0x10,
+			dataPreCRC[1] = 0x00,
+			dataPreCRC[2] = 0x00
+	};
+	uint8_t LED0O[] = {
+			LED0O[0] = dataMSG,
+			LED0O[1] = 0x10,
+			LED0O[2] = doskaADDR,
+			LED0O[3] = 0x01,
+			LED0O[4] = 0x00,
+			LED0O[5] = crc8_calc(dataPreCRC, sizeof(dataPreCRC))
+	};
+			LPSCI_WriteBlocking(DEMO_LPSCI, LED0O, sizeof(LED0O));
+}
+
+void led0INZhasni() {
+	uint8_t dataPreCRC[] = {
+			dataPreCRC[0] = 0x20,
+			dataPreCRC[1] = 0x00,
+			dataPreCRC[2] = 0x00
+	};
+	uint8_t LED0I[] = {
+			LED0I[0] = dataMSG,
+			LED0I[1] = 0x20,
+			LED0I[2] = doskaADDR,
+			LED0I[3] = 0x01,
+			LED0I[4] = 0x00,
+			LED0I[5] = crc8_calc(dataPreCRC, sizeof(dataPreCRC))
+		};
+				LPSCI_WriteBlocking(DEMO_LPSCI, LED0I, sizeof(LED0I));
+}
+
+void led1OUTZhasni() {
+	uint8_t dataPreCRC[] = {
+			dataPreCRC[0] = 0x11,
+			dataPreCRC[1] = 0x00,
+			dataPreCRC[2] = 0x00
+	};
+	uint8_t LED0O[] = {
+			LED0O[0] = dataMSG,
+			LED0O[1] = 0x11,
+			LED0O[2] = doskaADDR,
+			LED0O[3] = 0x01,
+			LED0O[4] = 0x00,
+			LED0O[5] = crc8_calc(dataPreCRC, sizeof(dataPreCRC))
+	};
+			LPSCI_WriteBlocking(DEMO_LPSCI, LED0O, sizeof(LED0O));
+}
+
+void led1INZhasni() {
+	uint8_t dataPreCRC[] = {
+			dataPreCRC[0] = 0x21,
+			dataPreCRC[1] = 0x00,
+			dataPreCRC[2] = 0x00
+	};
+	uint8_t LED0I[] = {
+			LED0I[0] = dataMSG,
+			LED0I[1] = 0x21,
+			LED0I[2] = doskaADDR,
+			LED0I[3] = 0x01,
+			LED0I[4] = 0x00,
+			LED0I[5] = crc8_calc(dataPreCRC, sizeof(dataPreCRC))
+		};
+				LPSCI_WriteBlocking(DEMO_LPSCI, LED0I, sizeof(LED0I));
+}
+
+void led2OUTZhasni() {
+	uint8_t dataPreCRC[] = {
+			dataPreCRC[0] = 0x12,
+			dataPreCRC[1] = 0x00,
+			dataPreCRC[2] = 0x00
+	};
+	uint8_t LED0O[] = {
+			LED0O[0] = dataMSG,
+			LED0O[1] = 0x12,
+			LED0O[2] = doskaADDR,
+			LED0O[3] = 0x01,
+			LED0O[4] = 0x00,
+			LED0O[5] = crc8_calc(dataPreCRC, sizeof(dataPreCRC))
+	};
+			LPSCI_WriteBlocking(DEMO_LPSCI, LED0O, sizeof(LED0O));
+}
+
+void led2INZhasni() {
+	uint8_t dataPreCRC[] = {
+			dataPreCRC[0] = 0x22,
+			dataPreCRC[1] = 0x00,
+			dataPreCRC[2] = 0x00
+	};
+	uint8_t LED0I[] = {
+			LED0I[0] = dataMSG,
+			LED0I[1] = 0x22,
+			LED0I[2] = doskaADDR,
+			LED0I[3] = 0x01,
+			LED0I[4] = 0x00,
+			LED0I[5] = crc8_calc(dataPreCRC, sizeof(dataPreCRC))
+		};
+				LPSCI_WriteBlocking(DEMO_LPSCI, LED0I, sizeof(LED0I));
+}
+
+void led3OUTZhasni() {
+	uint8_t dataPreCRC[] = {
+			dataPreCRC[0] = 0x13,
+			dataPreCRC[1] = 0x00,
+			dataPreCRC[2] = 0x00
+	};
+	uint8_t LED0O[] = {
+			LED0O[0] = dataMSG,
+			LED0O[1] = 0x13,
+			LED0O[2] = doskaADDR,
+			LED0O[3] = 0x01,
+			LED0O[4] = 0x00,
+			LED0O[5] = crc8_calc(dataPreCRC, sizeof(dataPreCRC))
+	};
+			LPSCI_WriteBlocking(DEMO_LPSCI, LED0O, sizeof(LED0O));
+}
+
+void led3INZhasni() {
+	uint8_t dataPreCRC[] = {
+			dataPreCRC[0] = 0x23,
+			dataPreCRC[1] = 0x00,
+			dataPreCRC[2] = 0x00
+	};
+	uint8_t LED0I[] = {
+			LED0I[0] = dataMSG,
+			LED0I[1] = 0x23,
+			LED0I[2] = doskaADDR,
+			LED0I[3] = 0x01,
+			LED0I[4] = 0x00,
+			LED0I[5] = crc8_calc(dataPreCRC, sizeof(dataPreCRC))
+		};
+				LPSCI_WriteBlocking(DEMO_LPSCI, LED0I, sizeof(LED0I));
+}
+
+void led4OUTZhasni() {
+	uint8_t dataPreCRC[] = {
+			dataPreCRC[0] = 0x14,
+			dataPreCRC[1] = 0x00,
+			dataPreCRC[2] = 0x00
+	};
+	uint8_t LED0O[] = {
+			LED0O[0] = dataMSG,
+			LED0O[1] = 0x14,
+			LED0O[2] = doskaADDR,
+			LED0O[3] = 0x01,
+			LED0O[4] = 0x00,
+			LED0O[5] = crc8_calc(dataPreCRC, sizeof(dataPreCRC))
+	};
+			LPSCI_WriteBlocking(DEMO_LPSCI, LED0O, sizeof(LED0O));
+}
+
+void led4INZhasni() {
+	uint8_t dataPreCRC[] = {
+			dataPreCRC[0] = 0x24,
+			dataPreCRC[1] = 0x00,
+			dataPreCRC[2] = 0x00
+	};
+	uint8_t LED0I[] = {
+			LED0I[0] = dataMSG,
+			LED0I[1] = 0x24,
+			LED0I[2] = doskaADDR,
+			LED0I[3] = 0x01,
+			LED0I[4] = 0x00,
+			LED0I[5] = crc8_calc(dataPreCRC, sizeof(dataPreCRC))
+		};
+				LPSCI_WriteBlocking(DEMO_LPSCI, LED0I, sizeof(LED0I));
+}
 
 /*!
  * @brief Main function
@@ -316,25 +621,54 @@ int main(void)
        	    		acknowledgmentSprava(message[2]);
        	    		delay(150);
        	    		if(message[2] == 0xc0){
-       	    			motorDole();
-       	    			delay(1500);
-       	    		}
-       	    		if(message[2] == 0xb0){
        	    			led0IN();
-       	    		}
-       	    		if(message[2] == 0xb4){
+       	    			delay(20);
        	    			led0OUT();
        	    		}
-       	    		if(message[2] == 0xc2){
-       	    			destinacia = true;
-       	    			motorHore();
+       	    		if(message[2] == 0xb0){
+       	    			led0INZhasni();
+       	    			delay(20);
+       	         	    led0OUTZhasni();
        	    		}
-       	    		if((message[2] == 0xe2) && (destinacia = true)){
-          	    		motorDole();
+       	    		if(message[2] == 0xc1){
+       	    			led1IN();
+       	    			delay(20);
+       	    			led1OUT();
+       	    		}
+       	    		if(message[2] == 0xb1){
+       	    			led1INZhasni();
+       	    			delay(20);
+       	    			led1OUTZhasni();
+       	    		}
+       	    		if(message[2] == 0xc2){
+       	    			led2IN();
+       	    			delay(20);
+       	    			led2OUT();
+       	    		}
+       	    		if(message[2] == 0xb2){
+       	    			led2INZhasni();
+       	    			delay(20);
+       	    			led2OUTZhasni();
+       	    		}
+       	    		if(message[2] == 0xc3){
+       	    		    led3IN();
+       	    		    delay(20);
+       	    		    led3OUT();
+       	    		}
+       	    		if(message[2] == 0xb3){
+       	    		    led3INZhasni();
+       	    		    delay(20);
+       	    		    led3OUTZhasni();
        	    		}
        	    		if(message[2] == 0xc4){
-       	    			motorHore();
-       	    			delay(1500);
+       	    		     led4IN();
+       	    		     delay(20);
+       	    		     led4OUT();
+       	    		}
+       	    		if(message[2] == 0xb4){
+       	    		     led4INZhasni();
+       	    		     delay(20);
+       	    		     led4OUTZhasni();
        	    		}
        	    		rxIndex=0;
        	    		messageComplete = false;
